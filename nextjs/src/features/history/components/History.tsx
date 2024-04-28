b20deabedcb9cecf76afc4364a6813a87bd2630b
Naoki Ishimura <mirontest1460@gmail.com>
2024-04-28 11:52:32 +0900

    リファクタリング

--- nextjs/src/features/history/components/History.tsx
+++ nextjs/src/features/history/components/History.tsx
@@ -48,7 +48,7 @@ export const History = ({ id }: Props) => {
         setIsLoading(true);
         const history = await getHistory(id) as HistoryItem[];
         setHistory(history);
-        await new Promise((resolve) => setTimeout(resolve, 3000));
+        await new Promise((resolve) => setTimeout(resolve, 1000));
         setIsLoading(false);
     }
 
